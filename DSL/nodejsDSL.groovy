9ff10ad34db77dd5fa912de2fb070c9a516c12f5
ivanstiment <120344290+ivanstiment@users.noreply.github.com>
2023-12-21 12:18:59 +0100

    Update nodejsDSL.groovy
    Modify GitHub Account

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/ivanstiment/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('ivanstiment')
+            node / gitConfigEmail('ivansostiment@gmail.com')
         }
     }
     triggers {
